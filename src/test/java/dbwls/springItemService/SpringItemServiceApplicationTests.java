cdf737b7d6261882a45de06b88e9e4457e5e6227
Hanadoggy <78478981+Hanadoggy@users.noreply.github.com>
2023-06-16 11:01:45 +0900

    Initial commit

---
+++ src/test/java/dbwls/springItemService/SpringItemServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dbwls.springItemService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringItemServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
